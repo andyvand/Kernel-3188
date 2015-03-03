3f4f536fe8f5ec854b9986d263638b369381c95f
superceleron <superceleron@sapo.pt>
2015-03-03 23:59:26 +0000

    commit updated

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 175 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#175 SMP PREEMPT Sat Dec 20 18:30:11 WET 2014"
+#define LINUX_COMPILE_BY "celeron"
+#define LINUX_COMPILE_HOST "celeron-VirtualBox"
+#define LINUX_COMPILER "gcc version 4.8.2 (Ubuntu/Linaro 4.8.2-16ubuntu4) "
